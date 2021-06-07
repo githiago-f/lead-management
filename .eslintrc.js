58266b69ca31041b93d1d9b2db5872b6645188d6
githiago-f <tfarias@protonmail.com>
2021-06-07 20:57:08 -0300

    cofigure and initialize application and tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,21 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'semi': [
+      'error',
+      'always'
+    ],
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ]
   },
 };
