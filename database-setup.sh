baf80b8fa262bfd9d8ce0289d5882d512f7fe48f
githiago-f <tfarias@protonmail.com>
2021-06-08 12:23:31 -0300

    setup database provider and docker database

---
+++ database-setup.sh
@@ -0,0 +1,1 @@
+docker run -it --rm --env-file production.env -p 5432:5432 postgres
--- src/data/database.provider.ts
+++ src/data/database.provider.ts
@@ -8,28 +8,31 @@ const entities = [
 ];
 
 const databaseProviderMap = (env: Env) => {
-  if(env === 'production') {
+  switch(env) {
+  case 'production':
     return {
       provide: Constants.providers.db,
       useFactory: async () => createConnection({
         type: 'postgres',
-        database: '',
-        host: '',
-        password: '',
-        port: 5432,
-        username: '',
+        database: process.env.DB_NAME || 'application',
+        host: process.env.DB_HOST || 'localhost',
+        password: process.env.DB_PASSWORD || 'pass123',
+        port: parseInt(process.env.DB_PORT) || 5432,
+        username: process.env.DB_USERNAME||'guest',
+        synchronize: true,
+        entities
+      })
+    };
+  default:
+    return {
+      provide: Constants.providers.db,
+      useFactory: async () => createConnection({
+        type: 'sqlite',
+        database: ':memory:',
         entities
       })
     };
   }
-  return {
-    provide: Constants.providers.db,
-    useFactory: async () => createConnection({
-      type: 'sqlite',
-      database: ':memory:',
-      entities
-    })
-  };
 };
 
 export const databaseProviders: Provider[] = [databaseProviderMap(process.env.NODE_ENV as Env)];
