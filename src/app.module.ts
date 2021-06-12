ad739012ff347086fa280f47ffa030deb311bfa0
githiago-f <tfarias@protonmail.com>
2021-06-12 17:31:43 -0300

    import auth from auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
+import { AuthModule } from './auth/auth.module';
 import { Module } from '@nestjs/common';
 import { DomainModule } from 'src/domain/domain.module';
 import { DatabaseModule } from 'src/data/database.module';
 
 @Module({
   imports: [
+    AuthModule,
     DatabaseModule,
     DomainModule
   ],
